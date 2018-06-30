f7e9d5c592ca5af8e36746786134378e5cf103a2
SirG97 <sirgittarus@gmail.com>
2018-06-30 13:53:14 +0100

    caching assets returned from the network and displaying them

--- sw.js
+++ sw.js
@@ -30,7 +30,10 @@ self.addEventListener('activate', event =>{
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then( response => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).then(response => {
+                cache.put(event.request, response.clone());
+                return response;
+            });
         })
     );
 })
